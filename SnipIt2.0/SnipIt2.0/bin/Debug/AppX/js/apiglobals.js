e957d8221ddb43bf7873fbc525fcdd3dad2e8b45
Leandro Quezada <leandroquezada@me.com>
2013-11-17 01:40:23 -0500

    Adding Windows 8 App

---
+++ SnipIt2.0/SnipIt2.0/bin/Debug/AppX/js/apiglobals.js
@@ -0,0 +1,13 @@
+﻿var FoursquareAPIGlobals = function () {
+
+    // this will be a private property
+    var clientId = '55ZVWPA3THDF5DTCOF3ELHS0CXSGZCVFRND532BQRRWBXBB5';
+    var clientSecret = 'GOSDA4WP52I1MSS1DQZQVAWXCTTIUA04YWC4AAAQI5PLEW4V';
+
+    return {
+        // declare which properties and methods are supposed to be public
+        clientId: clientId,
+        clientSecret: clientSecret
+
+    }
+}();
---
+++ SnipIt2.0/SnipIt2.0/bin/Debug/AppX/js/data.js
@@ -0,0 +1,233 @@
+﻿
+var Foursquare = function () {
+    /*
+    Please register a client/app with foursquare ( https://developer.foursquare.com) 
+    and put the client id and secret in the apiglobals.js
+
+    NOTE: Didn't want to use require for this, but it would be easy to port it
+    and have its required dependancies be Jquery and apiglobals.js
+    */
+    var init = function (client_id, client_secret) {
+        clientId = '55ZVWPA3THDF5DTCOF3ELHS0CXSGZCVFRND532BQRRWBXBB5';// Required
+        clientSecret = 'GOSDA4WP52I1MSS1DQZQVAWXCTTIUA04YWC4AAAQI5PLEW4V';//Required
+        console.log(clientId); 
+        version = getVersionString();
+        defaultLngLat = '40.77,73.98';
+    }
+
+    /*The Foursquare api changes. Passing a "v" parameter on the call
+      means you are requesting the most recent from ths date. 
+      This creates a version string based on todays date
+    */
+    var getVersionString = function () {
+        // Foursquare wants the format YYYYMMDD
+        var today = new Date()
+        var day = today.getDate().toString();
+        var month = today.getMonth() + 1;
+        month = (month < 10) ? '0' + month.toString() : month.toString();
+        var year = today.getFullYear().toString();
+        var dateString = year + month + day;
+        return (dateString);
+    }
+
+    var endPoints = {
+        'getVenueDetailById': 'https://api.foursquare.com/v2/venues/',
+        'getVenueCategories': 'https://api.foursquare.com/v2/venues/categories',
+        'getPopularVenues': 'https://api.foursquare.com/v2/venues/explore',
+        'searchVenues': 'https://api.foursquare.com/v2/venues/search',
+        'getTrendingVenues': 'https://api.foursquare.com/v2/venues/trending',
+    }
+
+
+    var getVenueDetailById = function (venue_id) {
+        var url = endPoints['getVenueDetailById'] + venue_id;
+        return (makeJSONRequest(newUrl));
+    }
+
+
+    var getVenueCategories = function () {
+        var url = endPoints['getVenueCategories'];
+        return (makeJSONRequest(url));
+    }
+
+    /*
+    Please refer to https://developer.foursquare.com/docs/venues/explore for the option parameters that can be sent
+    */
+    var getPopularVenues = function (options) {
+        var url = endPoints['getPopularVenues'];
+        if (!options) {
+            options = { 'll': defaultLngLat };
+        }
+
+        return (makeJSONRequest(url, options));
+    }
+    //https://developer.foursquare.com/docs/venues/search
+    var searchVenues = function (options) {
+        // needs to be sorted
+        var url = endPoints['searchVenues'];
+        if (!options) {
+            options = { 'll': defaultLngLat };
+        }
+        return (makeJSONRequest(url, options));
+    }
+    var getTrendingVenues = function (options) {
+        if (!options) {
+            options = { 'll': defaultLngLat };
+        }
+        var url = endPoints['getTrendingVenues']
+
+        return (makeJSONRequest(url, options));
+    }
+
+
+    var makeJSONRequest = function (url, data) {
+        var self = this;
+        if (!data) { data = {} }
+
+        data.client_id = clientId;
+        data.client_secret = clientSecret;
+        data.v = version;
+
+        var jqxhr = $.getJSON(url, data);
+
+        return (jqxhr);
+
+    }
+
+    return {
+        // public properties
+        init: init,
+        getTrendingVenues: getTrendingVenues,
+        searchVenues: searchVenues,
+        getPopularVenues: getPopularVenues,
+        getVenueCategories: getVenueCategories
+    }
+}();
+
+(function () {
+    "use strict";
+    var list = new WinJS.Binding.List();
+    var groupedItems = list.createGrouped(
+        function groupKeySelector(item) { return item.group.key; },
+        function groupDataSelector(item) { return item.group; }
+    );
+    Foursquare.init(clientId, clientSecret);
+
+    // TODO: Replace the data with your real data.
+    // You can add data from asynchronous sources whenever it becomes available.
+    generateSampleData().forEach(function (item) {
+        list.push(item);
+    }); 
+
+    WinJS.Namespace.define("Data", {
+        items: groupedItems,
+        groups: groupedItems.groups,
+        getItemReference: getItemReference,
+        getItemsFromGroup: getItemsFromGroup,
+        resolveGroupReference: resolveGroupReference,
+        resolveItemReference: resolveItemReference
+    });
+
+    // Get a reference for an item, using the group key and item title as a
+    // unique reference to the item that can be easily serialized.
+    function getItemReference(item) {
+        return [item.group.key, item.title];
+    }
+
+    // This function returns a WinJS.Binding.List containing only the items
+    // that belong to the provided group.
+    function getItemsFromGroup(group) {
+        return list.createFiltered(function (item) { return item.group.key === group.key; });
+    }
+
+    // Get the unique group corresponding to the provided group key.
+    function resolveGroupReference(key) {
+        for (var i = 0; i < groupedItems.groups.length; i++) {
+            if (groupedItems.groups.getAt(i).key === key) {
+                return groupedItems.groups.getAt(i);
+            }
+        }
+    }
+
+    // Get a unique item from the provided string array, which should contain a
+    // group key and an item title.
+    function resolveItemReference(reference) {
+        for (var i = 0; i < groupedItems.length; i++) {
+            var item = groupedItems.getAt(i);
+            if (item.group.key === reference[0] && item.title === reference[1]) {
+                return item;
+            }
+        }
+    }
+
+1    // data list. 
+    function generateSampleData() {
+       
+        var itemContent = "<p>Curabitur class aliquam vestibulum nam curae maecenas sed integer cras phasellus suspendisse quisque donec dis praesent accumsan bibendum pellentesque condimentum adipiscing etiam consequat vivamus dictumst aliquam duis convallis scelerisque est parturient ullamcorper aliquet fusce suspendisse nunc hac eleifend amet blandit facilisi condimentum commodo scelerisque faucibus aenean ullamcorper ante mauris dignissim consectetuer nullam lorem vestibulum habitant conubia elementum pellentesque morbi facilisis arcu sollicitudin diam cubilia aptent vestibulum auctor eget dapibus pellentesque inceptos leo egestas interdum nulla consectetuer suspendisse adipiscing pellentesque proin lobortis sollicitudin augue elit mus congue fermentum parturient fringilla euismod feugiat</p><p>Curabitur class aliquam vestibulum nam curae maecenas sed integer cras phasellus suspendisse quisque donec dis praesent accumsan bibendum pellentesque condimentum adipiscing etiam consequat vivamus dictumst aliquam duis convallis scelerisque est parturient ullamcorper aliquet fusce suspendisse nunc hac eleifend amet blandit facilisi condimentum commodo scelerisque faucibus aenean ullamcorper ante mauris dignissim consectetuer nullam lorem vestibulum habitant conubia elementum pellentesque morbi facilisis arcu sollicitudin diam cubilia aptent vestibulum auctor eget dapibus pellentesque inceptos leo egestas interdum nulla consectetuer suspendisse adipiscing pellentesque proin lobortis sollicitudin augue elit mus congue fermentum parturient fringilla euismod feugiat</p><p>Curabitur class aliquam vestibulum nam curae maecenas sed integer cras phasellus suspendisse quisque donec dis praesent accumsan bibendum pellentesque condimentum adipiscing etiam consequat vivamus dictumst aliquam duis convallis scelerisque est parturient ullamcorper aliquet fusce suspendisse nunc hac eleifend amet blandit facilisi condimentum commodo scelerisque faucibus aenean ullamcorper ante mauris dignissim consectetuer nullam lorem vestibulum habitant conubia elementum pellentesque morbi facilisis arcu sollicitudin diam cubilia aptent vestibulum auctor eget dapibus pellentesque inceptos leo egestas interdum nulla consectetuer suspendisse adipiscing pellentesque proin lobortis sollicitudin augue elit mus congue fermentum parturient fringilla euismod feugiat</p><p>Curabitur class aliquam vestibulum nam curae maecenas sed integer cras phasellus suspendisse quisque donec dis praesent accumsan bibendum pellentesque condimentum adipiscing etiam consequat vivamus dictumst aliquam duis convallis scelerisque est parturient ullamcorper aliquet fusce suspendisse nunc hac eleifend amet blandit facilisi condimentum commodo scelerisque faucibus aenean ullamcorper ante mauris dignissim consectetuer nullam lorem vestibulum habitant conubia elementum pellentesque morbi facilisis arcu sollicitudin diam cubilia aptent vestibulum auctor eget dapibus pellentesque inceptos leo egestas interdum nulla consectetuer suspendisse adipiscing pellentesque proin lobortis sollicitudin augue elit mus congue fermentum parturient fringilla euismod feugiat</p><p>Curabitur class aliquam vestibulum nam curae maecenas sed integer cras phasellus suspendisse quisque donec dis praesent accumsan bibendum pellentesque condimentum adipiscing etiam consequat vivamus dictumst aliquam duis convallis scelerisque est parturient ullamcorper aliquet fusce suspendisse nunc hac eleifend amet blandit facilisi condimentum commodo scelerisque faucibus aenean ullamcorper ante mauris dignissim consectetuer nullam lorem vestibulum habitant conubia elementum pellentesque morbi facilisis arcu sollicitudin diam cubilia aptent vestibulum auctor eget dapibus pellentesque inceptos leo egestas interdum nulla consectetuer suspendisse adipiscing pellentesque proin lobortis sollicitudin augue elit mus congue fermentum parturient fringilla euismod feugiat</p><p>Curabitur class aliquam vestibulum nam curae maecenas sed integer cras phasellus suspendisse quisque donec dis praesent accumsan bibendum pellentesque condimentum adipiscing etiam consequat vivamus dictumst aliquam duis convallis scelerisque est parturient ullamcorper aliquet fusce suspendisse nunc hac eleifend amet blandit facilisi condimentum commodo scelerisque faucibus aenean ullamcorper ante mauris dignissim consectetuer nullam lorem vestibulum habitant conubia elementum pellentesque morbi facilisis arcu sollicitudin diam cubilia aptent vestibulum auctor eget dapibus pellentesque inceptos leo egestas interdum nulla consectetuer suspendisse adipiscing pellentesque proin lobortis sollicitudin augue elit mus congue fermentum parturient fringilla euismod feugiat</p><p>Curabitur class aliquam vestibulum nam curae maecenas sed integer cras phasellus suspendisse quisque donec dis praesent accumsan bibendum pellentesque condimentum adipiscing etiam consequat vivamus dictumst aliquam duis convallis scelerisque est parturient ullamcorper aliquet fusce suspendisse nunc hac eleifend amet blandit facilisi condimentum commodo scelerisque faucibus aenean ullamcorper ante mauris dignissim consectetuer nullam lorem vestibulum habitant conubia elementum pellentesque morbi facilisis arcu sollicitudin diam cubilia aptent vestibulum auctor eget dapibus pellentesque inceptos leo egestas interdum nulla consectetuer suspendisse adipiscing pellentesque proin lobortis sollicitudin augue elit mus congue fermentum parturient fringilla euismod feugiat";
+        var itemDescription = "Item Description: Pellentesque porta mauris quis interdum vehicula urna sapien ultrices velit nec venenatis dui odio in augue cras posuere enim a cursus convallis neque turpis malesuada erat ut adipiscing neque tortor ac erat";
+        var groupDescription = "Group Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tempor scelerisque lorem in vehicula. Aliquam tincidunt, lacus ut sagittis tristique, turpis massa volutpat augue, eu rutrum ligula ante a ante";
+
+        // These three strings encode placeholder images. You will want to set the
+        // backgroundImage property in your real data to be URLs to images.
+        var darkGray = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXY3B0cPoPAANMAcOba1BlAAAAAElFTkSuQmCC";
+        var lightGray = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXY7h4+cp/AAhpA3h+ANDKAAAAAElFTkSuQmCC";
+        var mediumGray = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXY5g8dcZ/AAY/AsAlWFQ+AAAAAElFTkSuQmCC";
+
+           
+        // Each of these sample groups must have a unique key to be displayed
+        // separately.
+        var groups = [
+            { key: "nearby_Events", title: "Nearby Events", subtitle: "Group Subtitle: 1", backgroundImage: darkGray, description: groupDescription },
+            { key: "subscribed_Events", title: "Subscribed Events", subtitle: "Group Subtitle: 2", backgroundImage: lightGray, description: groupDescription },
+            { key: "trending_Events", title: "Trending Events", subtitle: "Group Subtitle: 3", backgroundImage: mediumGray, description: groupDescription }
+            /*{ key: "group4", title: "Group Title: 4", subtitle: "Group Subtitle: 4", backgroundImage: lightGray, description: groupDescription },
+            { key: "group5", title: "Group Title: 5", subtitle: "Group Subtitle: 5", backgroundImage: mediumGray, description: groupDescription },
+            { key: "group6", title: "Group Title: 6", subtitle: "Group Subtitle: 6", backgroundImage: darkGray, description: groupDescription }*/
+        ];
+
+        // Each of these sample items should have a reference to a particular
+        // group.
+        var sampleItems = [
+            { group: groups[0], title: "Item Title: 1", subtitle: "Item Subtitle: 1", description: itemDescription, content: itemContent, backgroundImage: lightGray },
+            { group: groups[0], title: "Item Title: 2", subtitle: "Item Subtitle: 2", description: itemDescription, content: itemContent, backgroundImage: darkGray },
+            { group: groups[0], title: "Item Title: 3", subtitle: "Item Subtitle: 3", description: itemDescription, content: itemContent, backgroundImage: mediumGray },
+            { group: groups[0], title: "Item Title: 4", subtitle: "Item Subtitle: 4", description: itemDescription, content: itemContent, backgroundImage: darkGray },
+            { group: groups[0], title: "Item Title: 5", subtitle: "Item Subtitle: 5", description: itemDescription, content: itemContent, backgroundImage: mediumGray },
+
+            { group: groups[1], title: "Item Title: 1", subtitle: "Item Subtitle: 1", description: itemDescription, content: itemContent, backgroundImage: darkGray },
+            { group: groups[1], title: "Item Title: 2", subtitle: "Item Subtitle: 2", description: itemDescription, content: itemContent, backgroundImage: mediumGray },
+            { group: groups[1], title: "Item Title: 3", subtitle: "Item Subtitle: 3", description: itemDescription, content: itemContent, backgroundImage: lightGray },
+
+            { group: groups[2], title: "Item Title: 1", subtitle: "Item Subtitle: 1", description: itemDescription, content: itemContent, backgroundImage: mediumGray },
+            { group: groups[2], title: "Item Title: 2", subtitle: "Item Subtitle: 2", description: itemDescription, content: itemContent, backgroundImage: lightGray },
+            { group: groups[2], title: "Item Title: 3", subtitle: "Item Subtitle: 3", description: itemDescription, content: itemContent, backgroundImage: darkGray },
+            { group: groups[2], title: "Item Title: 4", subtitle: "Item Subtitle: 4", description: itemDescription, content: itemContent, backgroundImage: lightGray },
+            { group: groups[2], title: "Item Title: 5", subtitle: "Item Subtitle: 5", description: itemDescription, content: itemContent, backgroundImage: mediumGray },
+            { group: groups[2], title: "Item Title: 6", subtitle: "Item Subtitle: 6", description: itemDescription, content: itemContent, backgroundImage: darkGray },
+            { group: groups[2], title: "Item Title: 7", subtitle: "Item Subtitle: 7", description: itemDescription, content: itemContent, backgroundImage: mediumGray }
+
+            /*{ group: groups[3], title: "Item Title: 1", subtitle: "Item Subtitle: 1", description: itemDescription, content: itemContent, backgroundImage: darkGray },
+            { group: groups[3], title: "Item Title: 2", subtitle: "Item Subtitle: 2", description: itemDescription, content: itemContent, backgroundImage: lightGray },
+            { group: groups[3], title: "Item Title: 3", subtitle: "Item Subtitle: 3", description: itemDescription, content: itemContent, backgroundImage: darkGray },
+            { group: groups[3], title: "Item Title: 4", subtitle: "Item Subtitle: 4", description: itemDescription, content: itemContent, backgroundImage: lightGray },
+            { group: groups[3], title: "Item Title: 5", subtitle: "Item Subtitle: 5", description: itemDescription, content: itemContent, backgroundImage: mediumGray },
+            { group: groups[3], title: "Item Title: 6", subtitle: "Item Subtitle: 6", description: itemDescription, content: itemContent, backgroundImage: lightGray },
+
+            { group: groups[4], title: "Item Title: 1", subtitle: "Item Subtitle: 1", description: itemDescription, content: itemContent, backgroundImage: lightGray },
+            { group: groups[4], title: "Item Title: 2", subtitle: "Item Subtitle: 2", description: itemDescription, content: itemContent, backgroundImage: darkGray },
+            { group: groups[4], title: "Item Title: 3", subtitle: "Item Subtitle: 3", description: itemDescription, content: itemContent, backgroundImage: lightGray },
+            { group: groups[4], title: "Item Title: 4", subtitle: "Item Subtitle: 4", description: itemDescription, content: itemContent, backgroundImage: mediumGray },
+
+            { group: groups[5], title: "Item Title: 1", subtitle: "Item Subtitle: 1", description: itemDescription, content: itemContent, backgroundImage: lightGray },
+            { group: groups[5], title: "Item Title: 2", subtitle: "Item Subtitle: 2", description: itemDescription, content: itemContent, backgroundImage: darkGray },
+            { group: groups[5], title: "Item Title: 3", subtitle: "Item Subtitle: 3", description: itemDescription, content: itemContent, backgroundImage: mediumGray },
+            { group: groups[5], title: "Item Title: 4", subtitle: "Item Subtitle: 4", description: itemDescription, content: itemContent, backgroundImage: darkGray },
+            { group: groups[5], title: "Item Title: 5", subtitle: "Item Subtitle: 5", description: itemDescription, content: itemContent, backgroundImage: lightGray },
+            { group: groups[5], title: "Item Title: 6", subtitle: "Item Subtitle: 6", description: itemDescription, content: itemContent, backgroundImage: mediumGray },
+            { group: groups[5], title: "Item Title: 7", subtitle: "Item Subtitle: 7", description: itemDescription, content: itemContent, backgroundImage: darkGray },
+            { group: groups[5], title: "Item Title: 8", subtitle: "Item Subtitle: 8", description: itemDescription, content: itemContent, backgroundImage: lightGray }*/
+        ];
+
+        return sampleItems;
+    }
+})();
