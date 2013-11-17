e16f0a15aefa50787370da0f0602261bba06a179
Wayne Jones <wjones01@students.poly.edu>
2013-11-17 03:04:51 -0500

    Fixed Foursquare API issue

--- SnipIt2.0/SnipIt2.0/js/foursquare.js
+++ SnipIt2.0/SnipIt2.0/js/foursquare.js
@@ -7,17 +7,18 @@ var Foursquare = function () {
     NOTE: Didn't want to use require for this, but it would be easy to port it
     and have its required dependancies be Jquery and apiglobals.js
     */
-    var init = function (client_id, client_secret) {
+    var init = function (client_id,client_secret) {
         clientId = client_id;// Required
         clientSecret = client_secret;//Required
         version = getVersionString();
-        defaultLngLat = '40.77,73.98';
+        defaultLngLat = '40.7,-74';
     }
     /*The Foursquare api changes. Passing a "v" parameter on the call
       means you are requesting the most recent from ths date. 
       This creates a version string based on todays date
     */
-    var getVersionString = function () {
+    var getVersionString=function()
+    {
         // Foursquare wants the format YYYYMMDD
         var today = new Date()
         var day = today.getDate().toString();
@@ -25,7 +26,7 @@ var Foursquare = function () {
         month = (month < 10) ? '0' + month.toString() : month.toString();
         var year = today.getFullYear().toString();
         var dateString = year + month + day;
-        return (dateString);
+        return(dateString);
     }
 
     var endPoints = {
@@ -36,8 +37,9 @@ var Foursquare = function () {
         'getTrendingVenues': 'https://api.foursquare.com/v2/venues/trending',
     }
 
-
+    
     var getVenueDetailById = function (venue_id) {
+      
         var url = endPoints['getVenueDetailById'] + venue_id;
         return (makeJSONRequest(newUrl));
     }
@@ -52,11 +54,12 @@ var Foursquare = function () {
     Please refer to https://developer.foursquare.com/docs/venues/explore for the option parameters that can be sent
     */
     var getPopularVenues = function (options) {
+    
         var url = endPoints['getPopularVenues'];
         if (!options) {
             options = { 'll': defaultLngLat };
         }
-
+      
         return (makeJSONRequest(url, options));
     }
     //https://developer.foursquare.com/docs/venues/search
@@ -64,7 +67,7 @@ var Foursquare = function () {
         // needs to be sorted
         var url = endPoints['searchVenues'];
         if (!options) {
-            options = { 'll': defaultLngLat };
+            options = {'ll':defaultLngLat};
         }
         return (makeJSONRequest(url, options));
     }
@@ -73,21 +76,21 @@ var Foursquare = function () {
             options = { 'll': defaultLngLat };
         }
         var url = endPoints['getTrendingVenues']
-
+        
         return (makeJSONRequest(url, options));
     }
-
+   
 
     var makeJSONRequest = function (url, data) {
         var self = this;
         if (!data) { data = {} }
-
+      
         data.client_id = clientId;
         data.client_secret = clientSecret;
         data.v = version;
-
+       
         var jqxhr = $.getJSON(url, data);
-
+        
         return (jqxhr);
 
     }
@@ -96,9 +99,9 @@ var Foursquare = function () {
         // public properties
         init: init,
         getTrendingVenues: getTrendingVenues,
-        searchVenues: searchVenues,
+        searchVenues:searchVenues,
         getPopularVenues: getPopularVenues,
-        getVenueCategories: getVenueCategories
+        getVenueCategories:getVenueCategories
 
     }
 }();
